cf0ba21d6f0e71afde4e3ec1383f6ba94fee9295
pratik <pratikgaikwad551@gmail.com>
2020-09-02 11:52:24 +0530

    Updates

---
+++ precache-manifest.39881affe1e5e1db480e620cfbf1ceae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c6d61de1010b3f9c1235bab4fcf4e4",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "c102c2bf61b6865e6e2d",
+    "url": "/trello-clone/static/css/main.5671eba4.chunk.css"
+  },
+  {
+    "revision": "c3a126b6c338a6189328",
+    "url": "/trello-clone/static/js/2.5eae281d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/trello-clone/static/js/2.5eae281d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c102c2bf61b6865e6e2d",
+    "url": "/trello-clone/static/js/main.08ee2aa1.chunk.js"
+  },
+  {
+    "revision": "e86a24104ebac1f105b3",
+    "url": "/trello-clone/static/js/runtime-main.072ab029.js"
+  }
+]);
